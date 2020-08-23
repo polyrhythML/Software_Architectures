fb5d5efd8ff79e13b52cc217837bb5566c2ce623
polyrhythML <amit02093@gmail.com>
2020-08-23 22:10:58 +0530

    Adding the introductory script

---
+++ Introduction.py
@@ -0,0 +1,49 @@
+# This repository has the notes for the correct software architecture principles with Python.
+# This is an introductory script.
+
+"""
+What is an ARCHITECTURE ==>
+
+Architecture is the fundamental organization of a system embodied in its components, their relationships to each other
+and to the environment, and the principles guiding its design and evolution.
+
+SOFTWARE ARCHITECTURE VS DESIGN ==>
+
+* Architecture is higher level of the description, structures and interactions in a system.
+* Design is all about the organization of parts or the components of the system and the subsystems involved in making
+the system.
+
+ARCHITECTURE ==>
+
+* An architecture defines a structure
+* An architecture picks a core set of elements.
+* An architecture captures early design decisions.
+* An architecture manages the stakeholder's requirements.
+* An architecture influences the organisation/project team's structure.
+* An architecture is influenced by its environment.
+* An architecture documents the system.
+
+Why is Software Architecture is important?
+
+* Architecture selects quality attributes to be optimized for a system.
+* Architecture facilitates early prototyping.
+* Architecture allows a system to be built component-wise.
+* Architecture helps manage changes to the system.
+* If no architecture is defined, it becomes difficult to produce a system which is extensible and modifiable, and would
+not produce a system with a set of quality attributes quite far away from original requirements.
+
+What are Architecture's quality attributes ?
+
+A quality attribute is a measurable and testable property of a system which can be used to evaluate the performance of a
+system within its prescribed environment with respect to its non-functional aspects.
+
+* Modifiability
+* Testability
+* Scalability and performance
+* Availability
+* Security
+* Deploy-ability
+
+"""
+
+
